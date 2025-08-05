f278b46efc0826eac4f328e96c394928eed43760
Sabyasachi Mohanty <sabyasachim999@gmail.com>
2025-08-05 23:30:51 +0530

    Added some refactoring and code clean up

--- server.js
+++ server.js
@@ -1,11 +1,80 @@
-import net from 'net'
-import {parse} from './resp-parser.js'
+import net from 'net';
+import {parse} from './resp-parser.js';
+import saveRDB from './save.js';
 
-const dictionary = new Map()
+const dictionary = new Map();
 const server = net.createServer((socket) => {
     socket.on('data', (data) => {
-        const command = parse(data.toString())
-        console.log(command[0])
+        const command = parse(data.toString());
+        const [cmd, ...args] = command;
+        console.log(cmd)
+
+        if (!cmd) {
+            return sendError(socket, `Empty command`)
+        }
+
+        switch (cmd) {
+            case 'PING':
+                if (args.length === 0) sendSimpleString(socket, 'PONG');
+                else sendError(socket, 'Wrong number of arguments for PING');
+                break;
+            
+            case 'ECHO':
+                if (args.length === 1) sendBulkString(socket, args[0]);
+                else sendError(socket, 'Wrong number of arguments for ECHO');
+                break;
+
+            case 'SET':
+                if (args.length === 2) {
+                    dictionary.set(args[0], args[1]);
+                    sendSimpleString(socket, 'OK');
+                } else if (args.length === 4) {
+                    const [key, value, option, time] = args;
+                    dictionary.set(key, value);
+                    
+                    let timeout;
+                    if (option === 'EX') timeout = parseInt(time) * 1000;
+                    else if (option === 'PX') timeout = parseInt(time);
+                    else if (option === 'EXAT') timeout = parseInt(time) * 1000 - Date.now();
+                    else if (option === 'PXAT') timeout = parseInt(time) - Date.now();
+                    else return sendError(socket, 'Invalid expiration option');
+
+                    setTimeout(() => dictionary.delete(key), timeout);
+                    sendSimpleString(socket, 'OK');
+                } else {
+                    sendError(socket, 'Wrong number of arguments for SET');
+                }
+                break;
+
+            case 'GET':
+                if (args.length === 1) {
+                    const val = dictionary.get(args[0]);
+                    if (val !== undefined) sendBulkString(socket, String(val));
+                    else sendBulkString(socket, '');
+                } else {
+                    sendError(socket, 'Wrong number of arguments for GET')
+                }
+                break;
+
+            case 'EXISTS':
+                if (args.length === 1) sendInteger(socket, dictionary.has(args[0]) ? 1 : 0);
+                else sendError(socket, 'Wrong number of argument for EXISTS');
+                break;
+
+            case 'DEL':
+                if (args.length >= 1) {
+                    args.forEach((key) => dictionary.delete(key));
+                    sendSimpleString(socket, 'OK');
+                } else {
+                    sendError(socket, 'Wrong number of arguments for DEL');
+                }
+                break;
+
+            case 'INCR':
+            case 'DECR':
+                
+        }
+
         if (command.length === 1 && command[0] === 'PING') {
             socket.write('+PONG\r\n')
         } else if (command.length === 2 && command[0] === 'ECHO') {
@@ -99,6 +168,9 @@ const server = net.createServer((socket) => {
             } else {
                 socket.write(`-TYPEERROR INVALID FUNcTION`)
             }
+        } else if (command.length === 1 && command[0] === 'SAVE') {
+            saveRDB(dictionary)
+            
         } else {
             socket.write('+INVALID COMMAND\r\n')
         }
@@ -123,3 +195,20 @@ server.listen(PORT, (err) => {
     }
     console.log(`REDIS SERVER is successfully running on ${PORT}`)
 })
+
+// Reply Helpers
+function sendSimpleString(socket, str) {
+    socket.write(`+${str}\r\n`);
+}
+
+function sendBulkString(socket, str) {
+    socket.write(`\$${str.length}\r\n${str}\r\n`);
+}
+
+function sendInteger(socket, int) {
+    socket.write(`:${int}\r\n`);
+}
+
+function sendError(socket, msg) {
+    socket.write(`-ERR ${msg}\r\n`);
+}
